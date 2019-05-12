4f555ed5a3e7fe3082d862528cdd117bc0a8f2c0
Aditya Inapurapu <adityaii@gmail.com>
2019-05-12 14:44:02 -0700

    Single describe method for all tests. Assertion failure is still reported as a pass.

--- test/axios-sample-test.js
+++ test/axios-sample-test.js
@@ -2,19 +2,7 @@ const axios = require('axios');
 var assert = require('assert');
 var expect = require('chai').expect;
 
-describe('http-get', function () {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-        .then(response => {
-            expect(response.data.url).to.equal("https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg")
-            console.log(response.data.url);
-            console.log(response.data.explanation);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-});
-
-describe('Array', function() {
+describe('Tests', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal([1, 2, 3].indexOf(4), -1);
@@ -23,5 +11,19 @@ describe('Array', function() {
            assert.equal([1, 2, 3].indexOf(3), 2)
         });
     });
+
+    describe('#http-get', function () {
+        it('should be https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg', function () {
+           axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+                .then(response => {
+                    // assert.equal(response.data.url, 'https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg');
+                    expect(response.data.url).to.equal('https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jp');
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        });
+    });
 });
+
 

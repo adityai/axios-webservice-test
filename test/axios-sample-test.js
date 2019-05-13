984cc24145eee3e90ecb0851b1b323a496ba59d4
Aditya Inapurapu <adityaii@gmail.com>
2019-05-12 17:41:08 -0700

    Add pokemon api sample

--- test/axios-sample-test.js
+++ test/axios-sample-test.js
@@ -12,12 +12,26 @@ describe('Tests', function() {
         });
     });
 
-    describe('#http-get', function () {
-        it('should be https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg', function () {
-           axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    // describe('#nasa-get', function () {
+    //     it('should be https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg', function () {
+    //        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    //             .then(response => {
+    //                 // assert.equal(response.data.url, 'https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg');
+    //                 // expect(response.data.url).to.equal('https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg');
+    //             })
+    //             .catch(error => {
+    //                 console.log(error);
+    //             });
+    //     });
+    // });
+
+    // https://pokeapi.co/api/v2/pokemon/
+    describe('#pokemon-get', function () {
+        it('should be lightning-rod and url is https://pokeapi.co/api/v2/ability/31', function () {
+            axios.get('https://pokeapi.co/api/v2/pokemon/25')
                 .then(response => {
-                    // assert.equal(response.data.url, 'https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jpg');
-                    expect(response.data.url).to.equal('https://apod.nasa.gov/apod/image/1905/volcano_stefnisson_960.jp');
+                    expect(response.data.abilities[0].ability.name).to.equal('lightning-rod');
+                    expect(response.data.abilities[0].ability.url).to.equal('https://pokeapi.co/api/v2/ability/31/');
                 })
                 .catch(error => {
                     console.log(error);

a2de9be8c5cf23a139b96adb1cf73ba5c7e094a4
Aditya Inapurapu <adityaii@gmail.com>
2019-05-19 16:14:52 -0700

    json data-driven service test

--- test/axios-datadriven-test.js
+++ test/axios-datadriven-test.js
@@ -18,13 +18,33 @@ describe('Data-driven Tests', function() {
     describe('#json-datadriven-test', function (done) {
         var jsonString = file.readFileSync('test-data.json');
         var test_data = JSON.parse(jsonString);
-        // console.log(test_data);
         data_driven(test_data, function () {
             it('should return -1 when the json-data-driven value is not present', function (passedValue) {
                 softAssert.softAssert(-1, [1, 2, 3].indexOf(passedValue.value))
             });
         });
     });
-    
+
+    describe('#pokemon-datadriven-test', function (done) {
+        var jsonString = file.readFileSync('pokemon-data.json');
+        var test_data = JSON.parse(jsonString);
+        data_driven(test_data, function () {
+            test_data.forEach(function(element) {
+                console.log(element);
+                it('should be ' + element.expected, function () {
+                    return axios.get(element.uri)
+                        .then(response => {
+                            softAssert.softAssert(element.expected, response.data.abilities[0].ability.name)
+                        });
+                });
+            });
+            // it('should be ' + test_data[0].expected, function () {
+            //     return axios.get(test_data[0].uri)
+            //         .then(response => {
+            //             softAssert.softAssert(test_data[0].expected, response.data.abilities[0].ability.name)
+            //         });
+            // });
+        });
+    });
 });
 
